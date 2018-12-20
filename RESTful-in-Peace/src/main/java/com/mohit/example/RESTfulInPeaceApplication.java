b534f76cf13c9e2aed6f59bfc6ecec29ad1b2ca9
shadhin <shadhin@shadhin-HP-ProBook-440-G4>
2018-12-20 15:19:23 +0600

    removed unnecessary code

--- RESTful-in-Peace/src/main/java/com/mohit/example/RESTfulInPeaceApplication.java
+++ RESTful-in-Peace/src/main/java/com/mohit/example/RESTfulInPeaceApplication.java
@@ -1,25 +1,14 @@
 package com.mohit.example;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RESTfulInPeaceApplication extends SpringBootServletInitializer{
-
-	String contextPath = "";
+public class RESTfulInPeaceApplication{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(RESTfulInPeaceApplication.class, args);
 	}
 
-	@Override
-	public void onStartup(ServletContext servletContext) throws ServletException {
-		contextPath = servletContext.getContextPath();
-		super.onStartup(servletContext);
-	}
 }
 
